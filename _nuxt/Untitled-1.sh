8ca3310ef72b53439b53f84f9598e320fa2de80c
matthewkyleherren <99541432+matthewkyleherren@users.noreply.github.com>
2025-01-26 09:54:39 +0100

    sdbg

---
+++ _nuxt/Untitled-1.sh
@@ -0,0 +1,26 @@
+wget -nH -x https://mainfra.me/_nuxt/builds/meta/facb34ee-d799-4f8a-903b-11adbfc2ecb3.json
+
+[Error] Failed to load resource: the server responded with a status of 404 (Not Found) (enter-graphic-simple.svg, line 0)
+[Error] Failed to load resource: the server responded with a status of 404 (Not Found) (yzpynsiY.js, line 0)
+[Error] Failed to load resource: the server responded with a status of 404 (Not Found) (BJW067Ql.js, line 0)
+[Error] Failed to load resource: the server responded with a status of 404 (Not Found) (script.js, line 0)
+
+
+
+wget -nH -x https://mainfra.me/_nuxt/Bi6fD3zr.js;
+wget -nH -x https://mainfra.me/_nuxt/BznI95g4.js;
+wget -nH -x https://mainfra.me/_nuxt/yzpynsiY.js;
+wget -nH -x https://mainfra.me/_nuxt/BJW067Ql.js;
+wget -nH -x https://mainfra.me/_nuxt/C41yldak.js;
+wget -nH -x https://mainfra.me/_nuxt/BIo-1MJn.js;
+wget -nH -x https://mainfra.me/_nuxt/DhSUkxx_.js
+
+;
+
+
+
+wget -nH -x https://mainfra.me/image/enter-graphic-simple.svg;
+wget -nH -x https://mainfra.me/image/frameFallback.jpg;
+wget -nH -x https://mainfra.me/_vercel/insights/script.js;
+wget -nH -x https://mainfra.me/rive/tasks.riv;
+wget -nH -x https://mainfra.me/rive/send-button.riv
